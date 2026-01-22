cafe434ba6ac09b88dc958d769c83749478c984f
pranto paul <prantopaul55555@gmail.com>
2026-01-22 15:57:42 +0600

    Implement MongoDB connection and Note model

---
+++ note-crud/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.MONGO_URI;
+let isConnected = false;
+export default async function dbConnect() {
+  try {
+    if (!isConnected) {
+      const db = await mongoose.connect(MONGO_URI);
+      isConnected = db.connections[0].readyState === 1;
+      console.log('✅Connected to DB');
+    }
+  } catch (error) {
+    console.error('❌Can not connect to DB');
+  }
+}
